22cfea5df726cb0ea3e5d56bbf1079947b54272a
Austin <austin@catalyst.com.co>
2026-03-26 16:31:02 -0600

    Rename Create Blank Item to Create Ad Hoc

--- src/pages/Orders/OrderDetail.tsx
+++ src/pages/Orders/OrderDetail.tsx
@@ -960,7 +960,7 @@ export function OrderDetail() {
                    })}
                    className="gap-2 shrink-0 px-4 py-2 text-xs bg-black text-white hover:bg-neutral-800"
                  >
-                   <Plus size={14} /> Create Blank Item
+                   <Plus size={14} /> Create Ad Hoc
                  </PillButton>
                </div>
             </div>
